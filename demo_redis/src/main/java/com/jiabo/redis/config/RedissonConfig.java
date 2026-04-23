5ddcc03b2e3b8c7e311aeb369bf37811d57e0083
jianbo <896959814@qq.com>
2026-04-23 16:53:21 +0800

    1

--- demo_redis/src/main/java/com/jiabo/redis/config/RedissonConfig.java
+++ demo_redis/src/main/java/com/jiabo/redis/config/RedissonConfig.java
@@ -3,6 +3,7 @@ package com.jiabo.redis.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +15,7 @@ public class RedissonConfig {
      * @return
      */
     @Bean
+    @ConditionalOnMissingBean(RedissonClient.class)
     public RedissonClient initRedisson() {
         Config config = new Config();
         config.useSingleServer().setAddress("redis://127.0.0.1:6379");
