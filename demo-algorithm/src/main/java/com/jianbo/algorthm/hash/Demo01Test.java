8f8ca30650a3ea0a8de4822d22504cdbab9b8cf1
jianbo <896959814@qq.com>
2026-05-15 12:15:55 +0800

    feat: 实现哈希与链表算法题解

--- demo-algorithm/src/main/java/com/jianbo/algorthm/hash/Demo01Test.java
+++ demo-algorithm/src/main/java/com/jianbo/algorthm/hash/Demo01Test.java
@@ -3,7 +3,10 @@ package com.jianbo.algorthm.hash;
 import java.util.*;
 
 public class Demo01Test {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    Demo01Test demo01Test = new Demo01Test();
+    System.out.println(demo01Test.isHappy(19));
+  }
 
   // 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
   //
@@ -15,6 +18,18 @@ public class Demo01Test {
   // 有效的字母异位词
   public boolean isAnagram(String s, String t) {
     // todo 有效的字母异位词
+    int[] hash = new int[26];
+    for (int i = 0; i < s.length(); i++) {
+      hash[s.charAt(i) - 'a']++;
+    }
+    for (int i = 0; i < t.length(); i++) {
+      hash[t.charAt(i) - 'a']--;
+    }
+    for (int i : hash) {
+      if (i != 0) {
+        return false;
+      }
+    }
     return true;
   }
 
@@ -22,16 +37,40 @@ public class Demo01Test {
   // 输出结果中的每个元素一定是唯一的。 我们可以不考虑输出结果的顺序。
   // 两个数字的交集
   public int[] intersection(int[] nums1, int[] nums2) {
-    //todo 两个数字的交集 hashset
-    return null;
+    // todo 两个数字的交集 hashset
+    Set<Integer> set = new HashSet<>();
+    Set<Integer> res = new HashSet<>();
+    for (int i : nums1) {
+      set.add(i);
+    }
+    for (int i : nums2) {
+      if (set.contains(i)) {
+        res.add(i);
+      }
+    }
+    return res.stream().mapToInt(Integer::intValue).toArray();
   }
 
   // 1 <= nums1.length, nums2.length <= 1000
   // 0 <= nums1[i], nums2[i] <= 1000
   // hash解法
   public int[] intersection2(int[] nums1, int[] nums2) {
-    //todo 两个数字的交集 hash数组解法
-    return null;
+    // todo 两个数字的交集 hash数组解法
+    int[] hash1 = new int[1002];
+    int[] hash2 = new int[1002];
+    for (int i : nums1) {
+      hash1[i]++;
+    }
+    for (int i : nums2) {
+      hash2[i]++;
+    }
+    Set<Integer> resSet = new HashSet<>();
+    for (int i = 0; i < 1002; i++) {
+      if (hash1[i] > 0 && hash2[i] > 0) {
+        resSet.add(i);
+      }
+    }
+    return resSet.stream().mapToInt(Integer::intValue).toArray();
   }
 
   // 快乐数
@@ -40,12 +79,26 @@ public class Demo01Test {
   // 1，那么这个数就是快乐数。
   // 如果 n 是快乐数就返回 True ；不是，则返回 False 。
   public boolean isHappy(int n) {
-    //todo 快乐数
-    return false;
+    // todo 快乐数
+    Set<Integer> sums = new HashSet<>();
+    while (n > 0 && n != 1) {
+      sums.add(n);
+      n = getSum(n);
+      if (sums.contains(n)) {
+        return false;
+      }
+    }
+    return true;
   }
 
   public int getSum(int n) {
-    return 0;
+    int sum = 0;
+    while (n > 0) {
+      int yushu = n % 10;
+      sum += yushu * yushu;
+      n = n / 10;
+    }
+    return sum;
   }
 
   // 两数之和
@@ -54,7 +107,17 @@ public class Demo01Test {
   // 你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
   public int[] twoSum(int[] nums, int target) {
     int[] arr = new int[2];
-    //todo   两数之和
+    // todo   两数之和
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      int num = nums[i];
+      if (map.containsKey(target - num)) {
+        arr[0] = map.get(target - num);
+        arr[1] = i;
+        break;
+      }
+      map.put(num, i);
+    }
     return arr;
   }
 
@@ -72,7 +135,19 @@ public class Demo01Test {
   // 四数相加
   public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
     int res = 0;
-    //todo 四数相加
+    // todo 四数相加
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for (int i : nums1) {
+      for (int j : nums2) {
+        map.put(i + j, map.getOrDefault(i + j, 0) + 1);
+      }
+    }
+
+    for (int k : nums3) {
+      for (int l : nums4) {
+        res += map.getOrDefault(-k - l, 0);
+      }
+    }
     return res;
   }
 
@@ -83,7 +158,19 @@ public class Demo01Test {
   // 1 <= ransomNote.length, magazine.length <= 105
   // ransomNote 和 magazine 由小写英文字母组成
   public boolean canConstruct(String ransomNote, String magazine) {
-    //todo 赎金信
+    // todo 赎金信
+    int[] arr = new int[26];
+    for (int i = 0; i < magazine.length(); i++) {
+      arr[magazine.charAt(i) - 'a']++;
+    }
+    for (int i = 0; i < ransomNote.length(); i++) {
+      arr[ransomNote.charAt(i) - 'a']--;
+    }
+    for (int i : arr) {
+      if (i < 0) {
+        return false;
+      }
+    }
     return true;
   }
 
@@ -92,8 +179,31 @@ public class Demo01Test {
   // nums[j] + nums[k] == 0 。请你返回所有和为 0 且不重复的三元组。
   // 注意：答案中不可以包含重复的三元组
   public List<List<Integer>> threeSum(int[] nums) {
-    //todo 三数之和
-    return null;
+    // todo 三数之和
+    Arrays.sort(nums);
+    List<List<Integer>> res = new ArrayList<>();
+    int len = nums.length;
+    for (int a = 0; a < len - 2; a++) {
+      if (a > 0 && nums[a] == nums[a - 1]) continue;
+      if (nums[a] + nums[a + 1] + nums[a + 2] > 0) break;
+      if (nums[a] + nums[len - 1] + nums[len - 2] < 0) continue;
+      int b = a + 1, c = len - 1;
+      while (b < c) {
+        long sum = (long) nums[a] + nums[b] + nums[c];
+        if (sum == 0) {
+          res.add(Arrays.asList(nums[a], nums[b], nums[c]));
+          while (b < c && nums[b] == nums[b + 1]) b++;
+          while (b < c && nums[c] == nums[c - 1]) c--;
+          b++;
+          c--;
+        } else if (sum > 0) {
+          c--;
+        } else {
+          b++;
+        }
+      }
+    }
+    return res;
   }
 
   // 四数之和
@@ -104,7 +214,35 @@ public class Demo01Test {
   // nums[a] + nums[b] + nums[c] + nums[d] == target
   // 你可以按 任意顺序 返回答案
   public List<List<Integer>> fourSum(int[] nums, int target) {
-    //todo四数之和
-    return null;
+    // todo四数之和
+    List<List<Integer>> res = new ArrayList<>();
+    Arrays.sort(nums);
+    int len = nums.length;
+    for (int a = 0; a < len - 3; a++) {
+      if (a > 0 && nums[a] == nums[a - 1]) continue;
+      if ((long) nums[a] + nums[a + 1] + nums[a + 2] + nums[a + 3] > target) break;
+      if ((long) nums[a] + nums[len - 3] + nums[len - 2] + nums[len - 1] < target) continue;
+      for (int b = a + 1; b < len - 2; b++) {
+        if (b > a + 1 && nums[b] == nums[b - 1]) continue;
+        if ((long) nums[a] + nums[b] + nums[b + 1] + nums[b + 2] > target) break;
+        if ((long) nums[a] + nums[b] + nums[len - 1] + nums[len - 2] < target) continue;
+        int c = b + 1, d = len - 1;
+        while (c < d) {
+          long sum = (long) nums[a] + nums[b] + nums[c] + nums[d];
+          if (sum == target) {
+            res.add(Arrays.asList(nums[a], nums[b], nums[c], nums[d]));
+            while (c < d && nums[c] == nums[c + 1]) c++;
+            while (c < d && nums[d] == nums[d - 1]) d--;
+            c++;
+            d--;
+          } else if (sum < target) {
+            c++;
+          } else {
+            d--;
+          }
+        }
+      }
+    }
+    return res;
   }
 }
--- demo-algorithm/src/main/java/com/jianbo/algorthm/listnode/Demo01Test.java
+++ demo-algorithm/src/main/java/com/jianbo/algorthm/listnode/Demo01Test.java
@@ -1,38 +1,103 @@
 package com.jianbo.algorthm.listnode;
 
 public class Demo01Test {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    Demo01Test demo01Test = new Demo01Test();
+    demo01Test.removeElements(null, 0);
+  }
 
   //    给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
   public ListNode removeElements(ListNode head, int val) {
-    return null;
+    // todo 删除元素
+    ListNode listNode = new ListNode(0, head);
+    ListNode cur = listNode;
+    while (cur.next != null){
+      if (cur.next.val == val){
+        cur.next = cur.next.next;
+      }else {
+        cur = cur.next;
+      }
+    }
+    return listNode.next;
   }
 
   // 题意：反转一个单链表。
   // 示例: 输入: 1->2->3->4->5->NULL 输出: 5->4->3->2->1->NULL
   public ListNode reverseList(ListNode head) {
-    return null;
+    ListNode pre = null;
+    ListNode curr = head;
+    ListNode temp = null;
+    while (curr != null){
+      temp = curr.next;
+      curr.next = pre;
+      pre = curr;
+      curr = temp;
+    }
+    return pre;
   }
 
   //  给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
   public ListNode swapPairs(ListNode head) {
-    return null;
+    ListNode temp = new ListNode(0,head);
+    ListNode cur = temp;
+    while(cur.next != null && cur.next.next!= null){
+      ListNode n1 = cur.next;
+      ListNode n2 = cur.next.next;
+      n1.next = n2.next;
+      n2.next = n1;
+      cur.next = n2;
+      cur = cur.next.next;
+    }
+    return temp.next;
   }
 
   // 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
   // 进阶：你能尝试使用一趟扫描实现吗？
   public ListNode removeNthFromEnd(ListNode head, int n) {
-    return null;
+    ListNode listNode = new ListNode(0, head);
+    ListNode p1 = listNode;
+    ListNode p2 = listNode;
+    for(int i = 0; i < n+1; i++) {
+      p2 = p2.next;
+    }
+    while (p2!=null){
+      p1 = p1.next;
+      p2 = p2.next;
+    }
+    p1.next = p1.next.next;
+    return listNode.next;
   }
 
   // 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。
   public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-    return null;
+    ListNode p1 = headA;
+    ListNode p2 = headB;
+    while (p1!=p2){
+      if (p1 == null) p1 = headB;
+      else p1 = p1.next;
+      if (p2 == null) p2 = headA;
+      else p2= p2.next;
+    }
+    return p1;
   }
 
   // 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
   //思路 走1 走2 还原A  走1
   public ListNode detectCycle(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head;
+    while (fast != null && fast.next != null){
+      slow = slow.next;
+      fast = fast.next.next;
+      if (slow == fast){
+        slow = head;
+        while (slow != fast){
+          slow = slow.next;
+          fast = fast.next;
+        }
+        return slow;
+      }
+    }
     return null;
   }
 }
